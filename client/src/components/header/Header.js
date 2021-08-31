e824b1bac8725df9c84325e0bef2f350c661bd96
Moatez-km <moatez.kamounn@gmail.com>
2021-08-31 21:00:21 +0200

    header front

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 function Header() {
   return (
-    <div>
-      Header
-        
-    </div>
+    <header>
+        <div class="logo">
+            <h1><Link to= "/">Stagiaire</Link></h1>
+        </div>
+
+        <ul>
+            <li><Link to="/"><i class="fas fa-home"></i>Home</Link></li>
+            <li><Link to="/login"><i class="fas fa-user"></i>Sign in</Link></li>
+        </ul>
+
+
+    </header>
   );
 }
 
