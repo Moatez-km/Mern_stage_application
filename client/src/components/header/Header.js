73e9b753977cb47212ffa874b3bf7e7d411c6c01
Moatez-km <moatez.kamounn@gmail.com>
2021-09-01 19:08:48 +0200

    login page

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,13 +3,13 @@ import {Link} from 'react-router-dom'
 function Header() {
   return (
     <header>
-        <div class="logo">
+        <div className="logo">
             <h1><Link to= "/">Stagiaire</Link></h1>
         </div>
 
         <ul>
-            <li><Link to="/"><i class="fas fa-home"></i>Home</Link></li>
-            <li><Link to="/login"><i class="fas fa-user"></i>Sign in</Link></li>
+            <li><Link to="/"><i className="fas fa-home"></i>Home</Link></li>
+            <li><Link to="/login"><i className="fas fa-user"></i>Sign in</Link></li>
         </ul>
 
 
